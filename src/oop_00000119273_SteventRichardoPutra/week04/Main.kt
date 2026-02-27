28555be1c5781d9296e7b01be9e4481cabd9e377
sterep05 <steventrichardo020318@gmail.com>
2026-02-27 12:24:59 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000119273_SteventRichardoPutra/week04/Main.kt
+++ src/oop_00000119273_SteventRichardoPutra/week04/Main.kt
@@ -11,4 +11,10 @@ fun main() {
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("\n--- Testing Electric Car ---")
+    val mobilListrik = ElectricCar("BYD", 2, 100)
+    mobilListrik.openTrunk()
+    mobilListrik.honk()
+    mobilListrik.accelerate()
 }

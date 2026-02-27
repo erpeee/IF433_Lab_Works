03aa6f73655bd72a0ab2c81f4202bc82c6575d28
sterep05 <steventrichardo020318@gmail.com>
2026-02-27 12:46:04 +0700

    week04: (task 2) create Manager class overriding Employee

--- src/oop_00000119273_SteventRichardoPutra/week04/Manager.kt
+++ src/oop_00000119273_SteventRichardoPutra/week04/Manager.kt
@@ -6,7 +6,7 @@ open class Manager(name: String, baseSalary: Int): Employee(name, baseSalary) {
         println("$name sedang memimpin rapat divisi.")
     }
     override fun calculateBonus(): Int{
-        super.calculateBonus()
-        println("Anda mendapatkan bonus $calculateBonus + 500000")
+        super.calculateBonus() + 500000
+        println("Anda mendapatkan bonus $calculateBonus")
     }
 }

728ea898886729a94d2143f40555fbcbb1d98346
sterep05 <steventrichardo020318@gmail.com>
2026-03-27 14:28:19 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_00000119273_SteventRichardoPutra/week07/SystemStates.kt
+++ src/oop_00000119273_SteventRichardoPutra/week07/SystemStates.kt
@@ -1,11 +1,11 @@
 package oop_00000119273_SteventRichardoPutra.week07
 
 enum class SystemStates {
-    STARTING, RUNNING, STOPPPED
+    STARTING, RUNNING, STOPPED
 }
 
 sealed class ApiResponse {
-    data class Success(val data: DataUser) : ApiResponse()
+    data class Success(val data: String) : ApiResponse()
     data class Error(val message: String) : ApiResponse()
     object Loading : ApiResponse()
 }

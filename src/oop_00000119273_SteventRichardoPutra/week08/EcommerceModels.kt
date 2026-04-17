b386f92b6ae12d3f0150ec64b77be5d26520a660
sterep05 <steventrichardo020318@gmail.com>
2026-04-18 00:51:31 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000119273_SteventRichardoPutra/week08/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_00000119273_SteventRichardoPutra.week08
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()

3903186099d0267279189454e84fc48aa87efd1e
sterep05 <steventrichardo020318@gmail.com>
2026-04-18 00:56:22 +0700

    week08: (task) implement payment processing with !!

--- src/oop_00000119273_SteventRichardoPutra/week08/ApiParser.kt
+++ src/oop_00000119273_SteventRichardoPutra/week08/ApiParser.kt
@@ -3,26 +3,32 @@ package oop_00000119273_SteventRichardoPutra.week08
 class ApiParser {
 
     fun parseProduct(rawJson: Map<String, Any?>): Product? {
-        // Ekstrak id dan name dengan requireNotNull [cite: 200]
-        val id = requireNotNull(rawJson["id"] as? String) { "API Invalid: Missing ID" }[cite: 200]
+        val id = requireNotNull(rawJson["id"] as? String) { "API Invalid: Missing ID" }
         val name = requireNotNull(rawJson["name"] as? String) { "API Invalid: Missing Name" }
 
-        val type = rawJson["type"] as? String [cite: 203]
+        val type = rawJson["type"] as? String
 
         return when (type) {
             "ELECTRONIC" -> {
-                // Gunakan as? Int dengan fallback Elvis 12 [cite: 203]
                 val warranty = rawJson["warranty"] as? Int ?: 12
-                Electronic(id, name, warranty)[cite: 204]
+                Electronic(id, name, warranty)
             }
 
             "CLOTHING" -> {
-                // Gunakan as? String dengan fallback Elvis "All Size" [cite: 205]
                 val size = rawJson["size"] as? String ?: "All Size"
-                Clothing(id, name, size)[cite: 205]
+                Clothing(id, name, size)
             }
 
-            else -> null // Tipe tidak dikenal atau null [cite: 206]
+            else -> null
         }
+    }
+    fun checkout(product: Product) {
+        val id = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+
+        val transactionId = JavaPaymentService.processPayment(id)!!
+        println("Checkout Berhasil! ID Transaksi: $transactionId")
     }
 }

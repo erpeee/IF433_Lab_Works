9820d8e1a4bb0ee30bfec46fda302003d2360d0e
sterep05 <steventrichardo020318@gmail.com>
2026-04-18 00:53:26 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000119273_SteventRichardoPutra/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000119273_SteventRichardoPutra.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}

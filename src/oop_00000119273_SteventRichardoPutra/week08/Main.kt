341532ffb849b9d9bb9c37fce1ea7b28aa096361
sterep05 <steventrichardo020318@gmail.com>
2026-04-18 00:38:48 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_00000119273_SteventRichardoPutra/week08/Main.kt
+++ src/oop_00000119273_SteventRichardoPutra/week08/Main.kt
@@ -24,4 +24,15 @@ fun main {
         "Laptop",
         4500000.0
     )
+    for (item in mixedData) {
+        val text = item as? String
+
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
+
+    val someObject: Any = 100
+    val safeString = someObject as? String ?: "Unknown String"
+    println("Hasil cast + fallback: $safeString")
 }

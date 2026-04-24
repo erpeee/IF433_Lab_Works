1e00ad296c650a3b4d9c30123803572f77a0f47a
sterep05 <steventrichardo020318@gmail.com>
2026-04-24 13:18:13 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000119273_SteventRichardoPutra/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000119273_SteventRichardoPutra.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "OPEN"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 50, 120.0, "CLOSED"),
+        TradeLog("SOLUSDT", "SHORT", 20, -10.5, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 10, 25.0, "OPEN"),
+        TradeLog("BTCUSDT", "SHORT", 20, 0.0, "OPEN")
+    )
+}

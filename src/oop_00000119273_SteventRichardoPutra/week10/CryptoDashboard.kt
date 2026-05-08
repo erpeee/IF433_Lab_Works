ba61ca1d42d133c080aaacace7cf625b0ef357ad
sterep05 <steventrichardo020318@gmail.com>
2026-05-08 09:17:07 +0700

    week10: (task) populate coin repository

--- src/oop_00000119273_SteventRichardoPutra/week10/CryptoDashboard.kt
+++ src/oop_00000119273_SteventRichardoPutra/week10/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_00000119273_SteventRichardoPutra.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("Bitcoin", 65000.0))
+    coinRepo.add(Coin("DOGE",1.04))
+    coinRepo.add(Coin("Etherium", 4000.0))
+
 }

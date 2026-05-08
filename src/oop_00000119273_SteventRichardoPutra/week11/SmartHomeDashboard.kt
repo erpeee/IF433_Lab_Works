7c7cb8cad535c9d10d8430843a6bbf5309cea2c9
sterep05 <steventrichardo020318@gmail.com>
2026-05-08 14:59:24 +0700

    week11: (task) configure AC unit using run

--- src/oop_00000119273_SteventRichardoPutra/week11/SmartHomeDashboard.kt
+++ src/oop_00000119273_SteventRichardoPutra/week11/SmartHomeDashboard.kt
@@ -4,9 +4,9 @@ fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
 
     SmartDevice("", ""). apply {
-        name = "Philips WiZ Living Room",
-        category = "Lighting",
-        isOnline = true,
+        name = "Philips WiZ Living Room"
+        category = "Lighting"
+        isOnline = true
         powerLoad = 12
     }.also {
         homeDevices.add(it)
@@ -18,4 +18,12 @@ fun main() {
         println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
+    val acInverter = run {
+        val device = SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
+        device
+    }
+    homeDevices.add(acInverter)
+
+    homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
 }

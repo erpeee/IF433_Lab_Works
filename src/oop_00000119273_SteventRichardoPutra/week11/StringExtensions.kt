11c9dd94b855e23f025e1462ca174a5d117c01bf
sterep05 <steventrichardo020318@gmail.com>
2026-05-08 14:36:26 +0700

    week11: create string extension functions

---
+++ src/oop_00000119273_SteventRichardoPutra/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000119273_SteventRichardoPutra.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}

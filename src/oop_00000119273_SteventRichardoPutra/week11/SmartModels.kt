6ce908d74df067de7ede0e2e55cf3d40a4dbd079
sterep05 <steventrichardo020318@gmail.com>
2026-05-08 14:53:35 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_00000119273_SteventRichardoPutra/week11/SmartModels.kt
+++ src/oop_00000119273_SteventRichardoPutra/week11/SmartModels.kt
@@ -1,3 +1,8 @@
 package oop_00000119273_SteventRichardoPutra.week11
 
 data class SmartDevice(var name: String, var category: String, var isOnline: Boolean = false, var powerLoad: Int = 0)
+
+fun SmartDevice.diagnose(): String {
+    val status = if (isOnline) "Online" else "Offline"
+    return "[DIAGNOSTIK] $name | Kategori: $category | Status: $status | Daya: $powerLoad"
+}

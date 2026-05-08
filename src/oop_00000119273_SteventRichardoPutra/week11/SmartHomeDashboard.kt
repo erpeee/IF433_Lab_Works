5c38aac6cf0a336da1ec4ad4676f901f54dc140b
sterep05 <steventrichardo020318@gmail.com>
2026-05-08 15:00:23 +0700

    week11: (task) implement device search with let

--- src/oop_00000119273_SteventRichardoPutra/week11/SmartHomeDashboard.kt
+++ src/oop_00000119273_SteventRichardoPutra/week11/SmartHomeDashboard.kt
@@ -26,4 +26,8 @@ fun main() {
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
 
+    val searchResult = homeDevices.find { it.category == "Camera"}
+    searchResult?.let {
+        println("Hasil Pencarian: ${it.diagnose()}")
+    }
 }

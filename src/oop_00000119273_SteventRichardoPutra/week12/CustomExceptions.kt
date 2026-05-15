f595875ef0caaef8bd3fa20d1b2b5556cc4e635c
sterep05 <steventrichardo020318@gmail.com>
2026-05-15 14:37:07 +0700

    week12: implement multiple catch blocks

--- src/oop_00000119273_SteventRichardoPutra/week12/CustomExceptions.kt
+++ src/oop_00000119273_SteventRichardoPutra/week12/CustomExceptions.kt
@@ -8,12 +8,27 @@ class InsufficientFundsException(
 class BankAccount(var balance: Double) {
     fun withdraw(amount: Double) {
         if (amount < 0) {
-            throw InsufficientFundsException("Amount must be positive")
+            throw IllegalArgumentException("Amount must be positive")
         }
         if (amount > balance){
             throw InsufficientFundsException(amount, balance)
         }
         balance -= amount
         println("Withdrawal successful. Remaining balance: $balance")
+    }
+}
+
+fun main(){
+    println("=== Test Multiple Catch ===")
+    val account = BankAccount(100.0)
+
+    try{
+        account.withdraw(150.0)
+    } catch (e: InsufficientFundsException) {
+        println("Caught Domain Error: Uang tidak cukup. ${e.message}")
+    } catch (e: IllegalArgumentException) {
+        println("Caught Argument Error: Input tidak valid. ${e.message}")
+    } catch (e: Exception) {
+        println("Caught General Error: Terjadi kesalahan tidak terduga.")
     }
 }

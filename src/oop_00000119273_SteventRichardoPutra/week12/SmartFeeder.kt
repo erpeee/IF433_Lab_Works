773335edd1e41c53e374dbd310ef6d7c0fe9d13f
sterep05 <steventrichardo020318@gmail.com>
2026-05-15 15:02:02 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_00000119273_SteventRichardoPutra/week12/SmartFeeder.kt
+++ src/oop_00000119273_SteventRichardoPutra/week12/SmartFeeder.kt
@@ -1,5 +1,6 @@
 package oop_00000119273_SteventRichardoPutra.week12
 
+
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr"}
 
@@ -16,4 +17,18 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    try {
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+    }catch (e: DispenserJamException) {
+        println("Error: ${e.message}")
+    }catch (e: FoodEmptyException) {
+        println("Error: ${e.message}")
+    }catch(e: Exception) {
+        println("Error Umum: ${e.message}")
+        }
 }

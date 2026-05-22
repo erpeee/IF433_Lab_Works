a76df578c1f9a9956ab0d308e1a9d73765cce15c
sterep05 <steventrichardo020318@gmail.com>
2026-05-22 13:18:53 +0700

    week13: create Student model

---
+++ src/oop_00000119273_SteventRichardoPutra/week13/StudentSerialization.kt
@@ -0,0 +1,6 @@
+package oop_00000119273_SteventRichardoPutra.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(val name: String, val age: Int, val gpa: Double)

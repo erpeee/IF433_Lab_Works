ab69013c68e2be25bf7bb4b51299b13e4780d241
sterep05 <steventrichardo020318@gmail.com>
2026-05-29 16:03:42 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_00000119273_SteventRichardoPutra/week14/EcommerceTask.kt
+++ src/oop_00000119273_SteventRichardoPutra/week14/EcommerceTask.kt
@@ -1,5 +1,6 @@
 package oop_00000119273_SteventRichardoPutra.week14
 
+import oop_00000119273_SteventRichardoPutra.week07.GameItem
 import java.io.File
 
 class BadOrderProcessor {
@@ -17,4 +18,45 @@ class BadOrderProcessor {
         println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
     }
 
+}
+
+interface OrderRepository {
+    fun saveOrder(itemName: String, finalPrice: Double, cusutomerType: String)
+}
+
+class CsvOrderRepository : OrderRepository {
+    private val file = File("orders.csv")
+
+    override fun saveOrder(itemName: String, finalPrice: Double, cusutomerType: String) {
+        file.writer(append = true).use { writer ->
+            writer.write("$itemName, $finalPrice, $cusutomerType\n")
+        }
+    }
+}
+
+interface NotificationService {
+    fun sendNotification(itemName: String)
+}
+
+class EmailNotifier : NotificationService {
+    override fun sendNotification(itemName: String) {
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
+
+class SafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService,
+){
+    fun processOrder(itemName: String, basePrice: Double, cusutomerType: String) {
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP" -> basePrice * 0.90
+            else -> basePrice
+        }
+        println("Memproses pesanan $itemName seharga $finalPrice")
+
+        repo.saveOrder(itemName, finalPrice, cusutomerType)
+        notifier.sendNotification(itemName)
+    }
 }
